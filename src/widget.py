ed8143dc4468c0b7eb724b70aac115504269bf20
Arteemyev Mikhail <artemevmik1@tambler>
2026-05-05 21:42:29 +0500

    функция mask_akkaunt card

---
+++ src/widget.py
@@ -0,0 +1,13 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(number:str) -> str:
+    number_split = number.split(" ")
+    if len(number_split[-1]) == 16:
+        mask = get_mask_card_number(number_split[-1])
+    else:
+        mask = get_mask_account(number_split[-1])
+
+    number_mask = ' '.join(number_split[0:-1]) + ' ' + mask
+
+    return number_mask
