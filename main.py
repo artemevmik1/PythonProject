4efbd2524185487971a3a12b0b11b17c5c74a4a6
Arteemyev Mikhail <artemevmik1@tambler>
2026-05-13 22:46:52 +0500

    функция опциональная и функция сортировки

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import mask_account_card, get_date, get_datee
+from src.processing import filter_by_state, sort_by_date
+
 
 if __name__ == "__main__":
     print(get_mask_card_number("7000792289606361"))
@@ -13,3 +15,12 @@ if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 8990922113665229"))
     print(get_date("2024-03-11T02:26:18.671407"))
     print(get_datee("2024-03-11T02:26:18.671407"))
+
+
+    volues = [{'id': 414288297, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+    print(filter_by_state(volues ,'CANCELED'))
+    print(sort_by_date(volues, True))
