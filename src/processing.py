51ae5438a80c4d47fa3b65dff660f339098f6bbb
Arteemyev Mikhail <artemevmik1@tambler>
2026-05-17 09:22:43 +0500

    message

--- src/processing.py
+++ src/processing.py
@@ -19,5 +19,8 @@ def filter_by_state(data: List[Dict[str, Any]], state: Optional[str] = "EXECUTED
     return result
 
 
-def sort_by_date(list_of_dictionaries: list, sort: bool = True) -> list:
+def sort_by_date(list_of_dictionaries: list[Dict[str, Any]], sort: bool = True) -> list[Dict[str, Any]]:
+    """
+        Сортирует список словарей по ключу 'date'.
+    """
     return sorted(list_of_dictionaries, key=lambda d: d["date"], reverse=sort)
