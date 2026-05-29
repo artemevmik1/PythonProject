128376c89f12afb003886bc8e284ced7a2c66589
Arteemyev Mikhail <artemevmik1@tambler.ru>
2026-05-29 15:06:14 +0500

    тесты для функций

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict, List, Optional
-
+from datetime import datetime
 
 def filter_by_state(data: List[Dict[str, Any]], state: Optional[str] = "EXECUTED") -> List[Dict[str, Any]]:
     """
@@ -15,7 +15,10 @@ def filter_by_state(data: List[Dict[str, Any]], state: Optional[str] = "EXECUTED
 
     # Проверка типа входных данных
     if not isinstance(data, list):
-        raise TypeError(f"Ожидается список, получен")
+        raise TypeError("Ожидается список, получен другой тип")
+
+    if not data:
+        raise ValueError("Пустой список")
 
 
     result = []
@@ -29,4 +32,21 @@ def sort_by_date(list_of_dictionaries: list[Dict[str, Any]], sort: bool = True) 
     """
         Сортирует список словарей по ключу 'date'.
     """
+
+    if not isinstance(list_of_dictionaries, list):
+        raise TypeError("Ожидается список, получен другой тип")
+
+    if not list_of_dictionaries:
+        return []
+
+    for item in list_of_dictionaries:
+        if 'date' in item:
+            try:
+                datetime.fromisoformat(item['date'])
+            except (ValueError, TypeError):
+                # Можно либо пропустить, либо выбросить ошибку
+                raise ValueError(f"Некорректный формат даты: {item['date']}")
+
     return sorted(list_of_dictionaries, key=lambda d: d["date"], reverse=sort)
+
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -7,16 +7,38 @@ def number():
 
 
 @pytest.fixture
-def sample_data() -> List[Dict[str, Any]]:
+def sample_data():
     """Фикстура с тестовыми данными"""
     return [
         {"id": 1, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 2, "state": "PENDING", "date": "2018-10-14T08:21:33.419441"},
-        {"id": 3, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 2, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 3, "state": "PENDING", "date": "2018-09-12T21:27:25.241689"}
     ]
 
 
 @pytest.fixture
-def empty_data(self) -> List[Dict[str, Any]]:
+def empty_data():
     """Фикстура с пустым списком"""
     return []
+
+
+@pytest.fixture
+def list_time():
+    """Фикстура с датами"""
+    return [
+        "2019-07-03",
+        "2018-10-14",
+        "2018-09-12",
+        "2018-06-30"
+    ]
+
+
+@pytest.fixture
+def list_time_false():
+    """Фикстура с датами"""
+    return [
+        "2018-06-30",
+        "2018-09-12",
+        "2018-10-14",
+        "2019-07-03"
+    ]
